3cd97fd207fe199397ca891f72956844641cde34
rooberthh <evaldsson.roberth@gmail.com>
2025-11-06 20:35:04 +0100

    Fill methods for inMemory invoice gateway.

--- src/Services/InMemoryGateway.php
+++ src/Services/InMemoryGateway.php
@@ -3,19 +3,30 @@
 namespace Rooberthh\Faktura\Services;
 
 use Exception;
+use Illuminate\Support\Str;
 use Rooberthh\Faktura\Contracts\GatewayContract;
 use Rooberthh\Faktura\Models\Invoice;
 use Rooberthh\Faktura\Support\DataObjects\Invoice as InvoiceDTO;
+use Rooberthh\Faktura\Support\Enums\Provider;
 
 class InMemoryGateway implements GatewayContract
 {
     public function createInvoice(Invoice $invoice): InvoiceDTO
     {
-        throw new Exception('Not implemented');
+        $invoice->external_id = Str::uuid()->toString();
+        $invoice->provider = Provider::IN_MEMORY;
+        $invoice->save();
+
+        return InvoiceDTO::fromInvoice($invoice);
     }
 
     public function get(string $externalId): InvoiceDTO
     {
-        throw new Exception('Not implemented');
+        $invoice = Invoice::query()
+            ->where('provider', Provider::IN_MEMORY)
+            ->where('external_id', $externalId)
+            ->first();
+
+        return InvoiceDTO::fromInvoice($invoice);
     }
 }
