a466ddb6d20f6866d58a3505f20b2e857f5aad53
rooberthh <evaldsson.roberth@gmail.com>
2025-11-13 17:23:46 +0100

    Add more fixtures when when responding to stripe payloads.

--- src/Jobs/SyncInvoiceJob.php
+++ src/Jobs/SyncInvoiceJob.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueueAfterCommit;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use Rooberthh\Faktura\Models\Invoice;
 
@@ -28,5 +29,12 @@ class SyncInvoiceJob implements ShouldQueueAfterCommit
         $invoiceDTO = $invoice->gateway()->get($invoice->external_id);
 
         $invoice->syncFromDto($invoiceDTO);
+    }
+
+    public function middleware(): array
+    {
+        return [
+            new WithoutOverlapping((string) $this->invoiceId)
+        ];
     }
 }
